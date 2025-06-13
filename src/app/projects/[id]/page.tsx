2eab8bf58c79ad7ff9f4d0b450c967b1e7b748ad
Hakeem Lingo <hlingo3@gatech.edu>
2025-06-12 20:44:13 -0400

    add prop interface for project slug page

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,8 @@
+type ProjectPageProps = {
+  params: {
+    id: string;
+  };
+};
 export default function Page({ params }: { params: { id: string } }) {
   return <div>My slug is: {params.id}</div>;
 }
